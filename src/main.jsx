b509354f049c86a604e3669541e2f773fb6d96fa
Juan Simancas <dev.juansimancas@proton.me>
2023-07-10 04:39:34 -0500

    changed order

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import './App.css'
+import Clock from './module/time'
+import Data from './module/data'
+import Weather from './module/weather'
+import Pokemon from './module/pokemon'
+import SearchBar from './module/searchbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Weather/>
+      <Pokemon/>
+      <h1> Welcome!</h1>
+      <Clock/>
+      <Data/>
+      <SearchBar/>
   </React.StrictMode>,
 )
